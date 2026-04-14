c31b8f284f69ef5b7579f71afee729a7417921c7
keerthan-poovaiah <keerthan11poovaiah@gmail.com>
2026-04-14 19:55:09 +0530

    Improvised using web sockets

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+
+const http = require("http");
+const { Server } = require("socket.io");
 
 dotenv.config();
 connectDB();
@@ -16,17 +20,47 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("SyncTalk API running...");
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chats", chatRoutes);
 app.use("/api/messages", messageRoutes);
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let onlineUsers = {};
+
+io.on("connection", (socket) => {
+  socket.on("join", (userId) => {
+    onlineUsers[userId] = socket.id;
+  });
+
+  socket.on("send_message", (data) => {
+    const { receiverId } = data;
+
+    const receiverSocket = onlineUsers[receiverId];
+
+    if (receiverSocket) {
+      io.to(receiverSocket).emit("receive_message", data);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    for (let userId in onlineUsers) {
+      if (onlineUsers[userId] === socket.id) {
+        delete onlineUsers[userId];
+      }
+    }
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
