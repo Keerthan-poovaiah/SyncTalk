b620f33665e6e3a432cfdef2d74e63567bfca3f5
keerthan-poovaiah <keerthan11poovaiah@gmail.com>
2026-05-25 16:16:04 +0530

    Addede message bubble component

---
+++ client/src/components/MessageBubble.jsx
@@ -0,0 +1,34 @@
+function MessageBubble({
+  msg,
+  currentUser,
+}) {
+  const senderId =
+    typeof msg.senderId === "object"
+      ? msg.senderId._id
+      : msg.senderId;
+
+  const isSender =
+    senderId === currentUser._id;
+
+  return (
+    <div
+      className={`flex mb-4 ${
+        isSender
+          ? "justify-end"
+          : "justify-start"
+      }`}
+    >
+      <div
+        className={`px-4 py-3 rounded-2xl text-white max-w-[60%] break-words ${
+          isSender
+            ? "bg-green-600 rounded-br-none"
+            : "bg-[#202c33] rounded-bl-none"
+        }`}
+      >
+        {msg.message}
+      </div>
+    </div>
+  );
+}
+
+export default MessageBubble;
