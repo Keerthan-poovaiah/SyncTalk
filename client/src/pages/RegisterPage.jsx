0abac37b9b555e5909dcb871ba973b5f98fccd69
keerthan-poovaiah <keerthan11poovaiah@gmail.com>
2026-05-26 08:24:06 +0530

    Created registration page

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate, Link } from "react-router-dom";
+
+function Register() {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleRegister = async () => {
+    try {
+      await API.post("/auth/register", form);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-[#111b21]">
+      <div className="bg-[#202c33] p-8 rounded-2xl w-[400px] shadow-lg">
+        <h1 className="text-3xl text-green-500 mb-6 text-center font-bold">
+          SyncTalk
+        </h1>
+
+        <input
+          type="text"
+          placeholder="Username"
+          className="w-full p-3 mb-4 rounded-lg bg-[#2a3942] text-white outline-none"
+          onChange={(e) =>
+            setForm({ ...form, username: e.target.value })
+          }
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full p-3 mb-4 rounded-lg bg-[#2a3942] text-white outline-none"
+          onChange={(e) =>
+            setForm({ ...form, email: e.target.value })
+          }
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-3 mb-4 rounded-lg bg-[#2a3942] text-white outline-none"
+          onChange={(e) =>
+            setForm({ ...form, password: e.target.value })
+          }
+        />
+
+        <button
+          onClick={handleRegister}
+          className="w-full bg-green-500 hover:bg-green-600 text-white p-3 rounded-lg transition"
+        >
+          Register
+        </button>
+
+        <p className="text-gray-400 mt-4 text-center">
+          Already have an account?{" "}
+          <Link to="/" className="text-green-500">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
